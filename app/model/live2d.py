38baf0cb8edf850c44fa071b4b81a5b74b6265a1
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2019-09-15 23:24:16 +0800

    Updated

--- app/model/live2d.py
+++ app/model/live2d.py
@@ -1,17 +1,13 @@
 from flask import url_for
-from app.mysql.live2d_db import live2dModel_db, live2dTip_db
-from app import base_dir
-import os, json
+from app.mysql.live2d_db import live2dModel_db, live2dTip_db,live2dConfig_db
+import os, json,urllib,requests
 
 
-def loadJsonFile(filedir, prefix="static"):
-    data = {}
+def loadJsonFile(url):
     try:
-        with open(os.path.join(base_dir, prefix, filedir), "r", encoding="utf-8") as f:
-            data = json.loads(f.read())
+        return requests.get(url).json()
     except:
-        pass
-    return data
+        return {}
 
 
 class live2dModel(object):
@@ -36,56 +32,57 @@ class live2dModel(object):
         return cls(model.id, model.name, model.path, textureId)
 
     def dump(self):
-        data = loadJsonFile(os.path.join(self.path,"model.json"))
-        textures = loadJsonFile(os.path.join(self.path,"textures.json"))
+        data = loadJsonFile(urllib.parse.urljoin(self.path,"model.json"))
+        textures = loadJsonFile(urllib.parse.urljoin(self.path,"textures.json"))
         try:
             textures = textures[self.textureId]
         except:
             textures = textures[0]
-        textures = list(map(lambda texture:"../../../../../.."+url_for("static",filename = os.path.join(self.path,texture)),textures))
+        textures = list(map(lambda texture:urllib.parse.urljoin(self.path,texture),textures))
 
         data["textures"] = textures
 
-        data["model"] = "../../../../../.."+url_for("static",filename = os.path.join(self.path,"model.moc"))
+
+        data["model"] = urllib.parse.urljoin(self.path,"model.moc")
+
 
         if not data.get("pose") is None:
-            data["pose"] = "../../../../../.." + url_for("static", filename=os.path.join(self.path, "pose.josn"))
+            data["pose"] = urllib.parse.urljoin(self.path,"pose.josn")
 
         if not data.get("motions") is None:
             for key in data["motions"].keys():
                 for item in data["motions"][key]:
-                    item["file"] = "../../../../../.."+url_for("static", filename=os.path.join(self.path, item["file"]))
+                    item["file"] = urllib.parse.urljoin(self.path, item["file"])
 
         if not data.get("expressions") is None:
             for item in data["expressions"]:
-                item["file"] = "../../../../../.."+url_for("static", filename=os.path.join(self.path, item["file"]))
+                item["file"] = urllib.parse.urljoin(self.path, item["file"])
 
 
         if not data.get("physics") is None:
-            data["physics"] = "../../../../../.."+url_for("static", filename=os.path.join(self.path, data["physics"]))
+            data["physics"] = urllib.parse.urljoin(self.path, data["physics"])
 
         return data
 
 
 class live2dConfig(object):
     def __init__(self, config="default", tip="default", model="kesshouban"):
+        if live2dConfig_db.get_by_name(tip) is None:
+            config = "default"
         self.name = config
+        self.configUrl = live2dConfig_db.get_by_name(tip).path
 
         if live2dTip_db.get_by_name(tip) is None:
             tip = "default"
-        self.tipsUrl = url_for("static", filename=live2dTip_db.get_by_name(tip).path)
+        self.tipsUrl = live2dTip_db.get_by_name(tip).path
 
         self.modelChangeApi = url_for("api.live2d-model-change")
         self.modelGetApi = url_for("api.live2d-model-get")
         self.model = live2dModel.initByName(model)
 
     def dump(self):
-        data = loadJsonFile("live2d/config/%s.json" % self.name)
-        if not bool(data):
-            data = loadJsonFile("live2d/config/default.json")
-
+        data = loadJsonFile(self.configUrl)
         data["tips"]["Url"] = self.tipsUrl
-
         data["model"]["ChangeApi"] = self.modelChangeApi
         data["model"]["GetApi"] = self.modelGetApi
         data["model"]["Id"] = self.model.id
--- app/mysql/live2d_db.py
+++ app/mysql/live2d_db.py
@@ -47,3 +47,24 @@ class live2dTip_db(db.Model):
         if rawdata is None:
             return None
         return rawdata
+
+class live2dConfig_db(db.Model):
+    __tablename__ = "live2dConfigs"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(16), nullable=False)
+    path = db.Column(db.String(128), nullable=False)
+    upload_time = db.Column(db.DateTime, index=True, default=bjutc)
+
+    @classmethod
+    def get_by_id(cls, id):
+        rawdata = cls.query.filter_by(id=id).first()
+        if rawdata is None:
+            return None
+        return rawdata
+
+    @classmethod
+    def get_by_name(cls, name):
+        rawdata = cls.query.filter_by(name=name).first()
+        if rawdata is None:
+            return None
+        return rawdata
